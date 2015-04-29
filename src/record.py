68785904d940470c932df67f528998a2fe719f59
Suguru Kato <salmon.bioinformatics@gmail.com>
2015-04-30 02:33:36 +0900

    Add record.py

---
+++ src/record.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from board import *
+from player import *
+
+class Record:
+
+    def __init__(self, game):
+        self._game = game
+        self._player_black = Player(Color.BLACK)
+        self._player_white = Player(Color.WHITE)
+        self._game.register_player(self._player_black)
+        self._game.register_player(self._player_white)
+
+    def step(self, string):
+        coord = Coordinate.from_str(string)
+        return self._game.put(self._game.get_turn(), coord)
+
+    def list_available(self):
+        return self._game._board.list_available(self._game._turn)
+
+
+def main():
+    from game import OthelloGame
+    game = OthelloGame()
+    record = Record(game)
+    print "Enter 'quit' to quit game"
+    while not game.has_finished():
+        print ""
+        print "Enter a coordinate"
+        candidates = map(lambda o:str(o),
+                game._board.list_available(game.get_turn().color))
+        print "Candidates: %s" % " ".join(candidates)
+        try:
+            print '>',
+            step = raw_input()
+            if step == "quit":
+                break
+            if record.step(step):
+                print game._board
+        except:
+            continue
+
+if __name__ == '__main__':
+    main()
